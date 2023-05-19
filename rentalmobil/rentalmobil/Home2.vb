38014df65e45d278976477e5208e17c926454947
Grace Ligit Nuh <56198370+graceln@users.noreply.github.com>
2023-05-19 13:25:48 +0800

    Delete Home2.vb

--- rentalmobil/rentalmobil/Home2.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Home2
-    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
-        Me.Hide()
-        Login2.Show()
-    End Sub
-End Class
